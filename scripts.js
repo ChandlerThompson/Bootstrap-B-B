3bef75235766b6ef63f750a2b06abc8f9671c591
Chandler Thompson <chandlerthompson2000@yahoo.com>
2021-07-25 11:30:57 -0500

    Upload 1

---
+++ scripts.js
@@ -0,0 +1,1 @@
+document.getElementById('theFooter').innerHTML = "<footer class='bg-dark text-light text-center'>Kailua Kove Bed & Breakfast 804 Mokapu Road • Kailua HI 96734  &#169; 2021</footer>";
